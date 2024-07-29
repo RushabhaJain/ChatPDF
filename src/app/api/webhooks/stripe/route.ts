4d127f3bddca96cec9e3a669d24c061cadd1368a
RushabhaJain <rushabhajain44@gmail.com>
2024-07-29 22:10:22 +0530

    changed the signature header

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -5,8 +5,8 @@ import type Stripe from 'stripe'
 
 export async function POST(request: Request) {
   const body = await request.text()
-  const signature = headers().get('Stripe-Signature') ?? ''
-
+  const signature = headers().get('stripe-signature') ?? ''
+  console.log(signature);
   let event: Stripe.Event
 
   try {
